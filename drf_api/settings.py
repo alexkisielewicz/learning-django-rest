8f1267cf2fbf7b88b61baed8ea5d33fe6f696011
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-05-24 15:17:54 +0000

    Change settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-if os.path.exists('env.py');
+if os.path.exists('env.py'):
     import env
     
 CLOUDINARY_STORAGE = {
@@ -31,7 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+)i6x1hr&c89!(p-5i8%w7je2w@17sy@yqrarl(_8yf4-0g&)z'
+SECRET_KEY = {
+    'SECRET_KEY': os.environ.get('SECRET_KEY')
+}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
